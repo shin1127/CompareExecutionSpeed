d6d62f52c183d82304c89cc798c61d134c264eb4
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-04-01 10:55:24 +0900

    誤字を修正、コメントを追加

--- SpeedTest.java
+++ SpeedTest.java
@@ -10,31 +10,31 @@ public class SpeedTest {
         boolean isSumExe = true;  // 加算による検証を行うかどうか
         int testCount = 10;  // 試行回数
         long sumCount = 1000000000;  // 1回の試行中の処理回数
-        long sumAvgTime = 0;
+        long sumAvgTime = 0;  // 加算処理の平均実行時間
 
-        // 円周率算出による処理時間計測
+        // 円周率近似による処理時間計測
         boolean isPiExe = true; // 円周率近似による検証を行うかどうか
         double pi = 0;  // 円周率の1/4の近似値
         double pi4 = 0;  // 円周率の近似値
         int piAccuracy = 100000000;  // 円周率算出の精度
         int piCount = 10;  // 円周率を算出する回数
-        long piAvgTime = 0;
+        long piAvgTime = 0;  // 円周率近似の平均実行時間
 
         // 加算による処理時間計測
         if (isSumExe){
             System.out.println("足し算による処理測定");
 
             for(int i = 0; i < testCount; i++){
-                // 開始
+                // 計測開始
                 long sumStartTime = System.currentTimeMillis();
                 
-                // {count}回の足し算による試行
+                // {count}回の加算による試行
                 int num = 0;
                 for(long j = 0; j < sumCount; j++){
                     num++;
                 }
                 
-                // 終了
+                // 計測終了
                 long sumEndTime = System.currentTimeMillis();
     
                 long sumRequiredTime = sumEndTime - sumStartTime;
@@ -45,14 +45,14 @@ public class SpeedTest {
 
         }
 
-        // 円周率算出による処理時間計測
+        // 円周率近似による処理時間計測
 
         if (isPiExe){
-            System.out.println("円周率算出による処理測定");
+            System.out.println("円周率近似による処理測定");
 
             for(int i = 0; i < piCount; i++){
     
-                // 開始
+                // 計測開始
                 long piStartTime = System.currentTimeMillis();
     
                 for(int j = 0; j < piAccuracy; j++){
@@ -60,7 +60,7 @@ public class SpeedTest {
                   }
                 pi4 = pi * 4;
     
-                // 終了
+                // 計測終了
                 long piEndTime = System.currentTimeMillis();
     
                 long piRequiredTime = piEndTime - piStartTime;
@@ -71,11 +71,11 @@ public class SpeedTest {
     
             // 加算処理の平均時間を求める
             sumAvgTime = calcAvg(sumTestDateList);
-            System.out.println("足し算試行の平均時間は" + sumAvgTime + " msec");
+            System.out.println("加算試行の平均時間は" + sumAvgTime + " msec");
     
             // 円周率算出の平均時間を求める
             piAvgTime = calcAvg(piTestDateList);
-            System.out.println("円周率算出試行の平均時間は" + piAvgTime + " msec");
+            System.out.println("円周率近似の平均時間は" + piAvgTime + " msec");
     
         }
 
